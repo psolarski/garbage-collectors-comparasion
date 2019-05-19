61246c35f12ba62f79a6e83e0b5a3707229be462
Piotr Solarski <piotrsolarski@outlook.com>
2019-05-19 15:21:25 +0200

    Added base configuration with Spring/gradle and swagger

---
+++ src/main/java/ps/pl/gc/GCComparison.java
@@ -0,0 +1,12 @@
+package ps.pl.gc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class GCComparison {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GCComparison.class, args);
+    }
+}
